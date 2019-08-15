36c5125cb2525c42f89c7b18c8463219bbf17c4b
harini srinivasan <harinsrini@gmail.com>
2019-08-15 16:37:11 +0200

    search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,36 @@ class ProductsController < ApplicationController
     @products_trek = Product.where(category: "Trekking")
     @products_cycle = Product.where(category: "cycling")
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+      if @product.save
+        redirect_to products_path #notice :"Restaurant created"
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    @product.user = current_user
+    @product.save
+    redirect_to products_path # index (single product) page
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category, :price_per_day, :photo, :photo_cache)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user
   validates :name, presence: true
   validates :description, presence: true
   validates :price_per_day, presence: true
-  validates :address, presence: true
 end
