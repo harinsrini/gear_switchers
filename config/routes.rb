ba4b138e7a796eb76f84f6e29ed4edfae9d3a6bf
harini srinivasan <harinsrini@gmail.com>
2019-08-16 15:27:50 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   root to: 'pages#home'
   resources :products
+  get "/dashboard", to: "pages#dashboard"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
